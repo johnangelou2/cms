cef3d5691bd73a0f1e274d640386ce2a6f4881d5
johnangelou2 <john.angelou@mail.utoronto.ca>
2022-10-25 11:49:03 -0400

    Added Residence table

---
+++ src/main/java/com/example/cms/model/entity/Residence.java
@@ -0,0 +1,23 @@
+package com.example.cms.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "residence")
+public class Residence {
+    @Id
+    private String address;
+
+    @NotEmpty
+    private String name;
+}
