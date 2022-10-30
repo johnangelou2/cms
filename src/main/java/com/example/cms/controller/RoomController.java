eeb8a1d2b3684e8b7b864433db463af813aad10b
johnangelou2 <john.angelou@mail.utoronto.ca>
2022-10-30 16:12:10 -0400

    Fixed retreave room Fuck Me

--- src/main/java/com/example/cms/controller/RoomController.java
+++ src/main/java/com/example/cms/controller/RoomController.java
@@ -1,6 +1,7 @@
 package com.example.cms.controller;
 
 import com.example.cms.controller.exceptions.RoomNotFoundException;
+import com.example.cms.controller.exceptions.StudentNotFoundException;
 import com.example.cms.model.entity.Room;
 import com.example.cms.model.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
