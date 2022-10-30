6a68f1e9ccf86eceffc556615687f35427a023f2
tongdav3 <102389546+tongdav3@users.noreply.github.com>
2022-10-30 16:12:12 -0400

    added admin and report controllers

--- src/main/java/com/example/cms/model/repository/ReportRepository.java
+++ src/main/java/com/example/cms/model/repository/ReportRepository.java
@@ -1,7 +1,15 @@
 package com.example.cms.model.repository;
 
+import com.example.cms.model.entity.Admin;
 import com.example.cms.model.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    @Query(value = "select * from reports r " +
+            "where lower(r.id) like lower(concat('%', :searchTerm, '%')) ", nativeQuery = true)
+    List<Report> search(@Param("searchTerm") String searchTerm);
 }
