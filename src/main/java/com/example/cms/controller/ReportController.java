b682126da046a68a9d5ba8c58f107f2914b73d9f
tongdav3 <102389546+tongdav3@users.noreply.github.com>
2022-10-30 15:47:03 -0400

    qqq

--- src/main/java/com/example/cms/controller/ReportController.java
+++ src/main/java/com/example/cms/controller/ReportController.java
@@ -1,4 +1,15 @@
 package com.example.cms.controller;
+//import com.example.cms.controller.exceptions.RoomNotFoundException;
+import com.example.cms.model.entity.Admin;
+import com.example.cms.model.entity.Student;
+import com.example.cms.model.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
 
 public class ReportController {
 }
