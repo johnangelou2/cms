317008d7190e68f662d3f41159f9177a7672d54c
Junwon Kang <kangjun1009@gmail.com>
2022-11-02 15:10:20 -0400

    Added student entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-INSERT INTO students (id, fullName, gender, dateOfBirth, phoneNum, email, emgPhoneNum, emgFullName, emgRelationship, probationStatus, stayStart, stayEnd) VALUES (1006722520, 'John Angelou', 'M', '20020401', '6476203772', 'johnangelou@uoft.ca', '6471111111', 'Mukund Hari', 'Daddy', 'Safe', '20220901', '20230501');
+INSERT INTO students (id, fullName, gender, dateOfBirth, phoneNum, email, emgPhoneNum, emgFullName, emgRelationship, probationStatus, stayStart, stayEnd) VALUES (1006722520, 'John Angelou', 'M', '20020401', '6476203772', 'johnangelou@uoft.ca', '6471111111', 'Mukund Hari', 'Father', 'Safe', '20220901', '20230501');
+INSERT INTO students (id, fullName, gender, dateOfBirth, phoneNum, email, emgPhoneNum, emgFullName, emgRelationship, probationStatus, stayStart, stayEnd) VALUES (1008982734, 'Jun Kang', 'M', '20001009', '6476202839', 'junkang@uoft.ca', '674819283', 'Martha Steward', 'Mother', 'Safe', '19982039', '19982737');
 
 INSERT INTO residences (id, address, name) VALUES (01, '78 Chestnut Road', 'Chestnut');
 INSERT INTO residences (id, address, name) VALUES (02, '27 Kings College Cir', 'University College');
