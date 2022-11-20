5e1e4943183b3adb5207d3a23b6f44846375fa2f
mkhi238 <100494918+mkhi238@users.noreply.github.com>
2022-11-20 16:58:20 -0500

    Reports

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,30 +1,64 @@
 <template>
   <div id="app">
     <b-container>
-    <div>
-      <b-navbar toggleable type="dark" variant="dark">
-      <b-navbar-brand href="#"><b-icon icon="book" variant="warning" style="margin-left: 20px;"></b-icon> Residential Management Services </b-navbar-brand>
+      <div>
+        <b-navbar toggleable type="dark" variant="dark">
+          <b-navbar-brand href="#"
+            ><b-icon
+              icon="book"
+              variant="warning"
+              style="margin-left: 20px;"
+            ></b-icon>
+            Residential Management Services
+          </b-navbar-brand>
 
-      <b-navbar-toggle target="sidebar-menu">
-      </b-navbar-toggle>
-      <b-sidebar id="sidebar-menu" title="RMS" backdrop-variant="dark" backdrop shadow>
-        <b-nav vertical align="center">
-          <b-nav-item to="/student-login" exact exact-active-class="exact-active">Home</b-nav-item>
-          <b-nav-item to="/admin-login" exact exact-active-class="exact-active">Admin Login</b-nav-item>
-          <b-nav-item to="/students" exact exact-active-class="exact-active">Students</b-nav-item>
-          <b-nav-item to="/residences" exact exact-active-class="exact-active">Residences</b-nav-item>
-          <b-nav-item disabled>Admin</b-nav-item>
-        </b-nav>
-      </b-sidebar>
-    </b-navbar>
-    </div>
-    <br />
-    <router-view/>
+          <b-navbar-toggle target="sidebar-menu"> </b-navbar-toggle>
+          <b-sidebar
+            id="sidebar-menu"
+            title="RMS"
+            backdrop-variant="dark"
+            backdrop
+            shadow
+          >
+            <b-nav vertical align="center">
+              <b-nav-item
+                to="/student-login"
+                exact
+                exact-active-class="exact-active"
+                >Home</b-nav-item
+              >
+              <b-nav-item
+                to="/admin-login"
+                exact
+                exact-active-class="exact-active"
+                >Admin Login</b-nav-item
+              >
+              <b-nav-item to="/students" exact exact-active-class="exact-active"
+                >Students</b-nav-item
+              >
+              <b-nav-item
+                to="/residences"
+                exact
+                exact-active-class="exact-active"
+                >Residences</b-nav-item
+              >
+              <b-nav-item
+                to="/residences"
+                exact
+                exact-active-class="exact-active"
+                >Reports</b-nav-item
+              >
+              <b-nav-item disabled>Admin</b-nav-item>
+            </b-nav>
+          </b-sidebar>
+        </b-navbar>
+      </div>
+      <br />
+      <router-view />
     </b-container>
-    
   </div>
 
-    <!--
+  <!--
 <ul>
         <li>
           <student-login></student-login>
@@ -38,33 +72,33 @@
       </main>
     </div>
   -->
-    </template>
+</template>
 
 <script>
-import StudentLogin from './components/StudentLogin.vue'
-import Residences from './components/Residences.vue'
-import AdminLogin from './components/AdminLogin.vue'
-import Students from './components/Students.vue'
+import StudentLogin from "./components/StudentLogin.vue";
+import Residences from "./components/Residences.vue";
+import AdminLogin from "./components/AdminLogin.vue";
+import Students from "./components/Students.vue";
+import Reports from "./components/Reports.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     StudentLogin,
     Residences,
+    Reports,
     Students
   }
-
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 20px;
-  
 }
 </style>
