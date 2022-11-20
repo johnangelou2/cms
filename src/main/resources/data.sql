d22da7c71d091e020e24ed271fe3f7f94d2ff625
tongdav3 <102389546+tongdav3@users.noreply.github.com>
2022-11-20 17:01:35 -0500

    added put for room and residence

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,6 +9,8 @@ INSERT INTO rooms (id, roomNum, roomType, capacity) VALUES (01, 102, 'single', 1
 INSERT INTO rooms (id, roomNum, roomType, capacity) VALUES (01, 103, 'double', 2);
 INSERT INTO rooms (id, roomNum, roomType, capacity) VALUES (01, 104, 'double', 2);
 
-INSERT INTO admins (id, firstName, lastName, gender, dateOfBirth, phoneNum, email, role, privilegeLevel, contractStart, contractEnd) VALUES(11111111, 'David', 'Tong', 'M', '20020408', '2509840533', 'dav.tong@mail.utoronto.ca', 'Manager', '4', '20200101', '20221220');
+INSERT INTO admins (id, firstName, lastName, gender, dateOfBirth, phoneNum, email, role, privilegeLevel, contractStart, contractEnd, name, reportId) VALUES(11111111, 'David', 'Tong', 'M', '20020408', '2509840533', 'dav.tong@mail.utoronto.ca', 'Floor 12 Don', '4', '20200101', '20221220', 'Chestnut', {01});
 INSERT INTO admins (id, firstName, lastName, gender, dateOfBirth, phoneNum, email, role, privilegeLevel, contractStart, contractEnd) VALUES(55555555, 'John', 'Angie', 'F', '19990201', '6471111122', 'admin@mail.utoronto.ca', 'Floor 14 Don', '4', '20220901', '20230530');
+
+INSERT INTO reports (id, date, time, status, lastName, roomNum) VALUES (01, '20221010', '040812', 'resolved', 'Tong', 101);
 
