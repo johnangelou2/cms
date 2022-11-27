a37b1ffd3940935210f38fe9593c61f7bccbe486
Minseok <73975626+Minseok0310@users.noreply.github.com>
2022-11-27 16:41:03 -0500

    Added info to Information chage

--- front-end/src/components/FurtherInformation.vue
+++ front-end/src/components/FurtherInformation.vue
@@ -1,5 +1,9 @@
 <template>
-<p>One of the most celebrated events in many people's lives is the transition towards higher education
+<div class="Information Page">
+    <h1>
+        Ajskdajdka
+    </h1>
+    <p>One of the most celebrated events in many people's lives is the transition towards higher education
     and moving away as one puruses their higher education. This web application aims to help this
     transition process by providing a feature suite in which those maintaining and living in residences
     can help manage their information.
@@ -14,4 +18,5 @@
     <!--
         For writing text, use loginpage as an example***
     -->
+</div>
 </template>
