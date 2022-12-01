6be17d1728ac4c7fe8bde481db901f8fe0af4b71
tongdav3 <102389546+tongdav3@users.noreply.github.com>
2022-11-30 20:53:14 -0500

    made date a date not a string

--- src/main/java/com/example/cms/controller/dto/ReportDto.java
+++ src/main/java/com/example/cms/controller/dto/ReportDto.java
@@ -6,13 +6,15 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import com.example.cms.model.entity.Admin;
 
+import java.sql.Date;
 import java.sql.Time;
 
 @Getter
 @Setter
 public class ReportDto {
     //date, time, desc, type, status, admin
-    String date;
+    @DateTimeFormat
+    Date date;
     long reportId;
     @DateTimeFormat
     Time time;
