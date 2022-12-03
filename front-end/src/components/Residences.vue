39ce361204949290835bca1a8eabdf5ba243896e
Cole Stotland <colestotland10@gmail.com>
2022-12-03 18:58:28 -0500

    Update Residences.vue

--- front-end/src/components/Residences.vue
+++ front-end/src/components/Residences.vue
@@ -4,21 +4,21 @@
     <p>&nbsp;&nbsp;</p>
     <h1 class="Head">Welcome to Opal Residential Managment Services!</h1>
     <p>&nbsp;&nbsp;</p>
-    <div class="wrapper">
-      <input checked type="radio" name="mover" id="spot1" />
-      <input type="radio" name="mover" id="spot2" />
-      <input type="radio" name="mover" id="spot3" />
-      <input type="radio" name="mover" id="spot4" />
-      <input type="radio" name="mover" id="spot5" />
+    <div class="cont">
+      <input checked type="radio" name="mover" id="part1" />
+      <input type="radio" name="mover" id="part2" />
+      <input type="radio" name="mover" id="part3" />
+      <input type="radio" name="mover" id="part4" />
+      <input type="radio" name="mover" id="part5" />
 
-      <div class="mover-wrapper">
-        <div class="inside">
+      <div class="mover-cont">
+        <div class="within">
           <article>
-            <div class="element">
-              <div class="info top-left">
+            <div class="part">
+              <div class="writing top-left">
                 <h3>Innis College</h3>
               </div>
-              <div class="info bottom-left">
+              <div class="writing bottom-left">
                 <h3>
                   A gender-inclusive, shared-apartment-style residence, and just
                   a few steps from Robarts Library!
@@ -34,11 +34,11 @@
           </article>
 
           <article>
-            <div class="element">
-              <div class="info top-left">
+            <div class="part">
+              <div class="writing top-left">
                 <h3>Chestnut Residence</h3>
               </div>
-              <div class="info bottom-left">
+              <div class="writing bottom-left">
                 <h3>
                   A residence comprised of themed communitites to suit many
                   interests, such as Engineering, Architecture, and more!
@@ -54,11 +54,11 @@
           </article>
 
           <article>
-            <div class="element">
-              <div class="info top-left">
+            <div class="part">
+              <div class="writing top-left">
                 <h3>University College</h3>
               </div>
-              <div class="info bottom-left">
+              <div class="writing bottom-left">
                 <h3>
                   A beautiful building that offers mostly single rooms in both
                   modern and historic residence halls
@@ -74,11 +74,11 @@
           </article>
 
           <article>
-            <div class="element">
-              <div class="info top-left">
+            <div class="part">
+              <div class="writing top-left">
                 <h3>New College</h3>
               </div>
-              <div class="info bottom-left">
+              <div class="writing bottom-left">
                 <h3>
                   New College is one of the youngest and most vibrant colleges
                   at U of T. It is also the largest and boasts the highest
@@ -95,11 +95,11 @@
           </article>
 
           <article>
-            <div class="element">
-              <div class="info top-left">
+            <div class="part">
+              <div class="writing top-left">
                 <h3>St. Michael's College</h3>
               </div>
-              <div class="info bottom-left">
+              <div class="writing bottom-left">
                 <h3>
                   Located on the east side of the University of Toronto’s
                   downtown campus and spanning almost 10 acres, the St.
@@ -116,9 +116,9 @@
             /></a>
           </article>
         </div>
-        <!-- .inside -->
+        <!-- .within -->
       </div>
-      <!-- .mover-wrapper -->
+      <!-- .mover-cont -->
 
       <!-- <div class = "arrowLeft">
   <img src = "https://static.vecteezy.com/system/resources/previews/008/844/878/original/arrow-icon-design-free-png.png"
@@ -137,20 +137,20 @@
  -->
 
       <div class="mover-prev-next-control">
-        <label for="spot1"></label>
-        <label for="spot2"></label>
-        <label for="spot3"></label>
-        <label for="spot4"></label>
-        <label for="spot5"></label>
+        <label for="part1"></label>
+        <label for="part2"></label>
+        <label for="part3"></label>
+        <label for="part4"></label>
+        <label for="part5"></label>
       </div>
       <!-- .mover-prev-next-control -->
 
       <div class="mover-dot-control">
-        <label for="spot1"></label>
-        <label for="spot2"></label>
-        <label for="spot3"></label>
-        <label for="spot4"></label>
-        <label for="spot5"></label>
+        <label for="part1"></label>
+        <label for="part2"></label>
+        <label for="part3"></label>
+        <label for="part4"></label>
+        <label for="part5"></label>
       </div>
       <!-- .mover-dot-control -->
     </div>
@@ -161,12 +161,8 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@charset "UTF-8";
-/*---- NUMBER OF SLIDE CONFIGURATION ----*/
-.Head {
-  text-align: center;
-}
-.wrapper {
+
+.cont {
   max-width: 60em;
   margin: 1em auto;
   position: relative;
@@ -196,7 +192,7 @@ input {
   display: none;
 }
 
-.inside {
+.within {
   width: 500%;
   line-height: 0;
 }
@@ -206,11 +202,13 @@ article {
   float: left;
   position: relative;
 }
+
+
 article img {
   width: 100%;
 }
 
-/*---- SET UP CONTROL ----*/
+/*---- how to control the slide ----*/
 .mover-prev-next-control {
   height: 50px;
   position: absolute;
@@ -256,8 +254,8 @@ article img {
   border-color: #777;
 }
 
-/* Info Box */
-.info {
+/* writing Box */
+.writing {
   position: absolute;
   font-style: italic;
   line-height: 20px;
@@ -268,32 +266,32 @@ article img {
   -moz-transition: all 1000ms ease-out 600ms;
   transition: all 1000ms ease-out 600ms;
 }
-.info h3 {
+.writing h3 {
   color: #fcfff4;
   margin: 0 0 5px;
   font-weight: normal;
   font-size: 1.5em;
   font-style: normal;
 }
-.info.top-left {
+.writing.top-left {
   top: 30px;
   left: 30px;
 }
-.info.top-right {
+.writing.top-right {
   top: 30px;
   right: 30px;
 }
-.info.bottom-left {
+.writing.bottom-left {
   bottom: 30px;
   left: 30px;
 }
-.info.bottom-right {
+.writing.bottom-right {
   bottom: 30px;
   right: 30px;
 }
 
 /* mover Styling */
-.mover-wrapper {
+.mover-cont {
   width: 100%;
   overflow: hidden;
   border-radius: 5px;
@@ -309,7 +307,7 @@ article img {
   -moz-transition: all 500ms ease-out;
   transition: all 500ms ease-out;
 }
-.mover-wrapper .inside {
+.mover-cont .within {
   -webkit-transform: translateZ(0);
   -moz-transform: translateZ(0);
   -ms-transform: translateZ(0);
@@ -320,22 +318,22 @@ article img {
   transition: all 800ms cubic-bezier(0.77, 0, 0.175, 1);
 }
 
-.element {
+.part {
   text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
     1px 1px 0 #000;
 }
 
 /*---- SET POSITION FOR SLIDE ----*/
-#spot1:checked ~ .mover-prev-next-control label:nth-child(5)::after,
-#spot5:checked ~ .mover-prev-next-control label:nth-child(4)::after,
-#spot4:checked ~ .mover-prev-next-control label:nth-child(3)::after,
-#spot3:checked ~ .mover-prev-next-control label:nth-child(2)::after,
-#spot2:checked ~ .mover-prev-next-control label:nth-child(1)::after,
-#spot5:checked ~ .mover-prev-next-control label:nth-child(1)::after,
-#spot4:checked ~ .mover-prev-next-control label:nth-child(5)::after,
-#spot3:checked ~ .mover-prev-next-control label:nth-child(4)::after,
-#spot2:checked ~ .mover-prev-next-control label:nth-child(3)::after,
-#spot1:checked ~ .mover-prev-next-control label:nth-child(2)::after {
+#part1:checked ~ .mover-prev-next-control label:nth-child(5)::after,
+#part5:checked ~ .mover-prev-next-control label:nth-child(4)::after,
+#part4:checked ~ .mover-prev-next-control label:nth-child(3)::after,
+#part3:checked ~ .mover-prev-next-control label:nth-child(2)::after,
+#part2:checked ~ .mover-prev-next-control label:nth-child(1)::after,
+#part5:checked ~ .mover-prev-next-control label:nth-child(1)::after,
+#part4:checked ~ .mover-prev-next-control label:nth-child(5)::after,
+#part3:checked ~ .mover-prev-next-control label:nth-child(4)::after,
+#part2:checked ~ .mover-prev-next-control label:nth-child(3)::after,
+#part1:checked ~ .mover-prev-next-control label:nth-child(2)::after {
   font-style: normal;
   font-weight: normal;
   text-decoration: inherit;
@@ -346,94 +344,74 @@ article img {
   color: rgba(119, 119, 119, 0);
 }
 
-#spot5:checked ~ .mover-prev-next-control label:nth-child(1)::after,
-#spot4:checked ~ .mover-prev-next-control label:nth-child(5)::after,
-#spot3:checked ~ .mover-prev-next-control label:nth-child(4)::after,
-#spot2:checked ~ .mover-prev-next-control label:nth-child(3)::after,
-#spot1:checked ~ .mover-prev-next-control label:nth-child(2)::after {
+#part5:checked ~ .mover-prev-next-control label:nth-child(1)::after,
+#part4:checked ~ .mover-prev-next-control label:nth-child(5)::after,
+#part3:checked ~ .mover-prev-next-control label:nth-child(4)::after,
+#part2:checked ~ .mover-prev-next-control label:nth-child(3)::after,
+#part1:checked ~ .mover-prev-next-control label:nth-child(2)::after {
   content: "";
   padding-left: 15px;
 }
 
-#spot5:checked ~ .mover-prev-next-control label:nth-child(1),
-#spot4:checked ~ .mover-prev-next-control label:nth-child(5),
-#spot3:checked ~ .mover-prev-next-control label:nth-child(4),
-#spot2:checked ~ .mover-prev-next-control label:nth-child(3),
-#spot1:checked ~ .mover-prev-next-control label:nth-child(2) {
+#part5:checked ~ .mover-prev-next-control label:nth-child(1),
+#part4:checked ~ .mover-prev-next-control label:nth-child(5),
+#part3:checked ~ .mover-prev-next-control label:nth-child(4),
+#part2:checked ~ .mover-prev-next-control label:nth-child(3),
+#part1:checked ~ .mover-prev-next-control label:nth-child(2) {
   display: block;
   float: right;
   margin-right: 5px;
 }
 
-#spot1:checked ~ .mover-prev-next-control label:nth-child(5),
-#spot5:checked ~ .mover-prev-next-control label:nth-child(4),
-#spot4:checked ~ .mover-prev-next-control label:nth-child(3),
-#spot3:checked ~ .mover-prev-next-control label:nth-child(2),
-#spot2:checked ~ .mover-prev-next-control label:nth-child(1) {
+#part1:checked ~ .mover-prev-next-control label:nth-child(5),
+#part5:checked ~ .mover-prev-next-control label:nth-child(4),
+#part4:checked ~ .mover-prev-next-control label:nth-child(3),
+#part3:checked ~ .mover-prev-next-control label:nth-child(2),
+#part2:checked ~ .mover-prev-next-control label:nth-child(1) {
   display: block;
   float: left;
   margin-left: 5px;
 }
 
-#spot1:checked ~ .mover-prev-next-control label:nth-child(5)::after,
-#spot5:checked ~ .mover-prev-next-control label:nth-child(4)::after,
-#spot4:checked ~ .mover-prev-next-control label:nth-child(3)::after,
-#spot3:checked ~ .mover-prev-next-control label:nth-child(2)::after,
-#spot2:checked ~ .mover-prev-next-control label:nth-child(1)::after {
+#part1:checked ~ .mover-prev-next-control label:nth-child(5)::after,
+#part5:checked ~ .mover-prev-next-control label:nth-child(4)::after,
+#part4:checked ~ .mover-prev-next-control label:nth-child(3)::after,
+#part3:checked ~ .mover-prev-next-control label:nth-child(2)::after,
+#part2:checked ~ .mover-prev-next-control label:nth-child(1)::after {
   content: "";
   padding-left: 8px;
 }
 
-#spot5:checked ~ .mover-dot-control label:nth-child(5),
-#spot4:checked ~ .mover-dot-control label:nth-child(4),
-#spot3:checked ~ .mover-dot-control label:nth-child(3),
-#spot2:checked ~ .mover-dot-control label:nth-child(2),
-#spot1:checked ~ .mover-dot-control label:nth-child(1) {
+#part5:checked ~ .mover-dot-control label:nth-child(5),
+#part4:checked ~ .mover-dot-control label:nth-child(4),
+#part3:checked ~ .mover-dot-control label:nth-child(3),
+#part2:checked ~ .mover-dot-control label:nth-child(2),
+#part1:checked ~ .mover-dot-control label:nth-child(1) {
   background: #333;
 }
 
-#spot5:checked ~ .mover-wrapper article:nth-child(5) .info,
-#spot4:checked ~ .mover-wrapper article:nth-child(4) .info,
-#spot3:checked ~ .mover-wrapper article:nth-child(3) .info,
-#spot2:checked ~ .mover-wrapper article:nth-child(2) .info,
-#spot1:checked ~ .mover-wrapper article:nth-child(1) .info {
+#part5:checked ~ .mover-cont article:nth-child(5) .writing,
+#part4:checked ~ .mover-cont article:nth-child(4) .writing,
+#part3:checked ~ .mover-cont article:nth-child(3) .writing,
+#part2:checked ~ .mover-cont article:nth-child(2) .writing,
+#part1:checked ~ .mover-cont article:nth-child(1) .writing {
   opacity: 1;
 }
 
-#spot1:checked ~ .mover-wrapper .inside {
+#part1:checked ~ .mover-cont .within {
   margin-left: 0%;
 }
-#spot2:checked ~ .mover-wrapper .inside {
+#part2:checked ~ .mover-cont .within {
   margin-left: -100%;
 }
-#spot3:checked ~ .mover-wrapper .inside {
+#part3:checked ~ .mover-cont .within {
   margin-left: -200%;
 }
-#spot4:checked ~ .mover-wrapper .inside {
+#part4:checked ~ .mover-cont .within {
   margin-left: -300%;
 }
-#spot5:checked ~ .mover-wrapper .inside {
+#part5:checked ~ .mover-cont .within {
   margin-left: -400%;
 }
-/*---- TABLET ----*/
-@media only screen and (max-width: 850px) and (min-width: 450px) {
-  .mover-wrapper {
-    border-radius: 0;
-  }
-}
-/*---- MOBILE----*/
-@media only screen and (max-width: 450px) {
-  .mover-wrapper {
-    border-radius: 0;
-  }
 
-  .mover-wrapper .info {
-    opacity: 0;
-  }
-}
-@media only screen and (min-width: 850px) {
-  body {
-    padding: 0 80px;
-  }
-}
 </style>
